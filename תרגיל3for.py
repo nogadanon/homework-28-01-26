dcf6ed41935a07cb3ed4e3399750aa8e27c71bc5
Noga <nogakol1@gmail.com>
2026-02-05 10:03:22 +0200

    4.2.26

--- תרגיל3for.py
+++ תרגיל3for.py
@@ -1,7 +1,7 @@
 # START
 i = 0
 _up_to_16 = 0
-for _ in range(1, 10 + 1) :
+for _ in range(0, 10) :
     x = int(input('enter age: '))
     if x > 18 :
         break
@@ -19,7 +19,6 @@ for _ in range(1, 10 + 1) :
 
 print('ther is ',i , 'players')
 print('ther is', _up_to_16, 'players over 16 years old (Including 16)')
-
 
 
 
